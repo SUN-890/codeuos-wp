78245426f5770a9fb8096df0f64d96698c15900d
Mami Yonemoto <arch_mummy_@hotmail.com>
2022-09-12 17:23:11 +0900

    first commit

---
+++ WordPressTheme/front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="l-mv p-mv">
+	<div class="p-mv__img">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/kv-img-sp.jpg" alt="PCtop">
+	</div><!-- /.p-mv__img -->
+</div><!-- /.l-mv p-mv -->
+
+
+
+
+<?php get_footer(); ?>
---
+++ WordPressTheme/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https: //fonts.googleapis.com/css2？family = Montserrat:wght @ 400; 500; 700&family = Noto + Sans + JP:wght @ 400; 500; 700& display = swap " rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
+
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<header class="l-header p-header">
+		<p class="p-test">
+			projectが入ります
+		</p><!-- /.test -->
+		<p class="c-test">
+			componentが入ります
+		</p><!-- /.test -->
+		<p class="l-test">
+			layoutが入ります
+		</p><!-- /.test -->
+
+
+	</header>
